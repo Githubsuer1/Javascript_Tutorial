797bed49beecef01349923cda0c0f5278a009c58
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-01 21:17:02 +0000

    add whileloop

---
+++ basics_01/04whileLoops.js
@@ -0,0 +1,30 @@
+//loops are very powerful as they have the ability to repeat a code to many times as we want in our program.
+
+
+// print first 10 natural num 
+let num1 = 10;
+let i = 1
+while (i<=num1){
+    console.log(i);
+    i++;
+}
+
+
+// print first 10 natural num in reverse order
+let num2 = 10;
+while (num2){
+    console.log(num2);
+    num2--;
+}
+
+
+// print first 10 even num 
+let num3 = 10;
+let j = 1;
+while (j<=num3){
+    console.log(i*2);
+    i++;
+}
+
+
+
