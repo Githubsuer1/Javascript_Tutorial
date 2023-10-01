e3def49b099cd9cb4c693963b4b299885743d127
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-01 20:22:36 +0000

    conditions check

---
+++ basics/conditionals.js
@@ -0,0 +1,39 @@
+// if else statements are used to work with many alternatives, if works only if condition true otherwise else exicuted.
+
+// simple if else statement
+let age = 60;
+if (age>60){
+    console.log("Yes you are eligible to get pention");
+}
+else{
+    console.log("Wait! You are not 60 year old.")
+}
+
+// if else if  statement
+
+let marks = 75;
+
+if (marks > 70){
+    console.log("Congratulations! you have got first division");
+}
+else if(marks > 55){
+    console.log("You have got second division.")
+}
+else if(marks > 33){
+    console.log("you are just passed with third division")
+}
+else{
+    console.log("oops! you are fail");
+}
+
+// even odd conditions 
+
+let num = 102;
+
+if (num%2==0){
+    console.log("num is even")
+}
+else{
+    console.log("num is odd");
+}
+
