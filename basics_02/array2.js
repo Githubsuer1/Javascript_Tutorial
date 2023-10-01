9407e42164f466914d4b1315aa96a5090c5fa2b0
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-01 15:31:24 +0000

    added new methods

--- basics_02/array2.js
+++ basics_02/array2.js
@@ -10,3 +10,16 @@ console.log(marval_heros);
 
 const all_heros = marval_heros.concat(dc_heros);
 console.log(all_heros);
+
+//accessing using spread operator
+//working of spread operator with two arrays is same as concatination 
+
+const all_new_heros = [...marval_heros, ...dc_heros];
+console.log(all_new_heros); 
+
+
+// flat method takes array as argument and returns all in a single array 
+
+const another_array = [1,2,3,4,[5,6,7],8,9,[10,[11,12]]];
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
