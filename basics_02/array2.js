c8014db8aee0ba340a3da65878c199f1f74441ab
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-01 15:50:14 +0000

    Added some new method

--- basics_02/array2.js
+++ basics_02/array2.js
@@ -18,8 +18,30 @@ const all_new_heros = [...marval_heros, ...dc_heros];
 console.log(all_new_heros); 
 
 
-// flat method takes array as argument and returns all in a single array 
+// flat method use with array (an array having another arrays as element) and returns all in a single array 
+// flat method does not modify existing array, it returns all elements in a another variabe.
 
 const another_array = [1,2,3,4,[5,6,7],8,9,[10,[11,12]]];
 const real_another_array = another_array.flat(Infinity);
 console.log(real_another_array);
+console.log(another_array);
+
+
+// isArray() method checks wheather a given value is array or not
+const data = "Keshavapuram";
+console.log(Array.isArray(data));
+
+// .from() method takes a value and convert it into a array, does not modify original array
+
+const createArray = Array.from(data);
+console.log(createArray);
+console.log(data);
+
+
+// .of() takes set of elements and rerurn a single array
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
+
