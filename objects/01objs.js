99821d7885c0d9ee64110c5e155470e56e6b7bb7
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-01 18:17:37 +0000

    Object creation

---
+++ objects/01objs.js
@@ -0,0 +1,20 @@
+// objects in javascript are used to store data in key value pair.
+// It can be created using literal and constructer
+// Behind the scene, keys are stored in form of strings, 
+
+// creating obj by literal
+
+const obj1 = {
+    name:"Rohit",
+    age:23,
+    address:"varanasi",
+    email:"rohit@chatgpt.com"
+}
+
+// accessing obj
+
+console.log(obj1);
+console.log(obj1.name);
+console.log(obj1["name"]);
+
+
