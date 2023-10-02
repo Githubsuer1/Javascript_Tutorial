ba5debb9573dd87eea78ad0c20d737b4edc75604
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-02 17:34:42 +0000

    operations

---
+++ data_types/03_operations.js
@@ -0,0 +1,24 @@
+// Operations 
+
+let value = 3
+
+let negativeValue = -value
+console.log(negativeValue)      // expected output = -3
+
+console.log(10+12)       // expected output = 22
+console.log(12 - 5)      // expected output = 7
+console.log(10*5)        // expected output = 50
+console.log(120/6)       // expected output = 20 
+console.log(7%3)         // expected output = 1
+console.log(2**2)        // expected output = 4
+console.log(2**3)        // expected output = 8
+
+console.log(10 + "22")       // expected output = 1022
+console.log("22" + 10)       // expected output = 2210
+console.log(10 + 22 + "21")  // expected output = 3221
+console.log("22" + 22 + 10)  // expected output = 222210
+console.log(1 + +"2")        // expected output = 3
+console.log(+"2" + 1)        // expected output = 3
+
+console.log(+true)       // expected output = 1
+console.log(+"")         // expected output = 0
