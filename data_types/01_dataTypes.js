18940a59cdd8b6b652e830fef8d0667a73317007
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-02 13:12:49 +0000

    dataTypes

---
+++ data_types/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";       //Treat all javaScript code as newer version
+
+// alert(3+3)       we are using nodejs not browser
+
+// premitive data types in javascript
+
+// numbers=> 2 to power 53
+// bigInt=> 
+// string=> " ",' '
+// boolean=> true/false
+// null=>  standalone value
+// undefined=> used as placeholder when we don't assign some value to a variable.
+// symbol=> used in advance javascript to uniquely difined values
+
+// non premitive data types
+
+// objects=> stores data in key value pair
+
+console.log(typeof null);               // Expected output:- object
+console.log(typeof undefined);          // Expected output:- undefined
+
+
+
+
