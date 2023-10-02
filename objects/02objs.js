9a1798591432cc61fdf924b7fd77ef2f1d41a0ff
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-02 06:06:21 +0000

    singleton obj created

---
+++ objects/02objs.js
@@ -0,0 +1,14 @@
+// singlton object by constructer
+
+// const tinderUser = new object();
+// console.log(tinderUser);
+
+const tinderUser = {}
+console.log(tinderUser);
+
+tinderUser.name = "Sammy";
+tinderUser.age = 25;
+tinderUser.location = "Vasai";
+tinderUser.role = "Devoloper";
+
+console.log(tinderUser);
