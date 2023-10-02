46901b0c4335a618f5cf2de17539ff59d6bf7643
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-02 10:23:34 +0000

    Todo list ready

--- Project1_ToDo_list/script.js
+++ Project1_ToDo_list/script.js
@@ -1,15 +1,43 @@
+// selecting input area by id method
 let inputText = document.getElementById("input1");
-let btnAdd = document.querySelector(".btnAdd");
 
-btnAdd.addEventListener("click",function(){
+// selecting button to add by id method
+let btnAdd = document.getElementById("btn1");
+
+//  adding eventListener to the button 
+btnAdd.addEventListener('click',function(){
+
+    // if user press add button without giving task
     if(inputText.value === ""){
         alert("Please Enter Task.")
     }
+
+    // if user press add button after giving task
     else{
+
+        // creating new element that will go to contain list items
         let newElement = document.createElement("ul");
-        newElement.innerHTML = `${inputText.value} <button class="btnRem">Remove</button>`;
+
+        // containing the content with btns
+        newElement.innerHTML = `${inputText.value} <button id="btn2" class="btnRem">Remove</button>`;
+
+        // the container that will contain newly created element
         let listContainer = document.querySelector(".listContainer");
-        listContainer.append(newElement);
+
+        // appending child element created above
+        listContainer.appendChild(newElement);
+
+        //making input box empty again
         inputText.value="";
+
+        //selecting remove btn by query selector
+        let remBtn = newElement.querySelector(".btnRem");
+
+        // adding event listener
+        remBtn.addEventListener('click',function(){
+
+            newElement.remove();   // removing task by remove method
+
+        })
     }
 })
