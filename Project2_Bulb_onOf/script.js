c3866c62205eda15392109ce301ab6fe0c2d4449
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-03 07:37:41 +0000

    Redy

--- Project2_Bulb_onOf/script.js
+++ Project2_Bulb_onOf/script.js
@@ -4,13 +4,12 @@ let btnon = document.getElementById("btnon");
 let body = document.getElementById("body");
 let box1 = document.querySelector(".box1");
 let container = document.querySelector(".container");
-let heading = document.getElementsByTagName("h1");
 
 // temp variable flag
 let flag = 1;
 
 
-// adding eventlistener to the btn
+// adding eventlistener to the button
 btnon.addEventListener("click", function(){
     if (flag == 1){
         body.style.backgroundColor = "white";
@@ -18,7 +17,6 @@ btnon.addEventListener("click", function(){
         btnon.style.backgroundColor = "red";
         box1.style.backgroundColor = "orange";
         container.style.backgroundColor = "white"
-        heading.style.color = "black";
         flag = 0;
     }
     else{
@@ -26,8 +24,7 @@ btnon.addEventListener("click", function(){
         btnon.textContent = "On";
         btnon.style.backgroundColor = "greenyellow";
         box1.style.backgroundColor = "rgb(35, 33, 33)";
-        container.style.backgroundColor = "rgb(41, 39, 39)";
-        // heading.style.color = "white";
-        flag = 1;
+        container.style.backgroundColor = "rgb(41, 39, 39)"
+        flag = 1
     }
 })
