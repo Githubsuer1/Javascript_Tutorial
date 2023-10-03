c9d89aace7d630aa28551e312ecd27770dec1635
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-03 05:57:05 +0000

    summary

---
+++ data_types/05_Summary.js
@@ -0,0 +1,56 @@
+// Premitive data types
+
+// String, Number, Undefined, Null, Boolean, Symbol and BigInt
+// These above data types are like call by value means the modification happened with there copy of values not by reference
+
+//null type
+let nullVariable = null
+console.log(typeof nullVariable)        // expected output:- object
+
+
+// undefined type
+let undefinedVariable;
+console.log(typeof undefinedVariable);  // expected output:- undefined
+console.log(undefinedVariable);         // expected output:- undefined
+
+
+//symbol type
+let symbol = Symbol('123');
+console.log(typeof symbol)          // expected output:- symbol
+
+let symbol1 = Symbol('123');
+console.log(typeof symbol);         // expected output:- symbol
+
+console.log(symbol === symbol1)     // expected output:- false
+
+
+// Non premitive (Reference)
+
+// Array, Objects , Functions 
+
+let array1 = ["name","age",123]
+
+console.log(typeof array1)          // expected output:- object
+console.log(array1[0])
+
+// Object
+let obj1 = {
+    name:"sidd",
+    age:23,
+    location:"varanasi"
+}
+
+console.log(typeof obj1)            // expected output:- object
+console.log(obj1)
+
+
+// Function Expression
+const myFunction = function xyz(){
+    console.log("hello world!");
+}
+
+//  xyz();                      // expected output:- xyz() not defined because it is stored in a variable as value,
+
+console.log(typeof myFunction)  // expected output:- function
+console.log(myFunction);        // expected output:- [function : xyz]
+myFunction()                    // expected output:- hello world!
