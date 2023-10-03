0139bf9a4794b09df1310e4925d2ead9325b8d4c
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-03 06:23:33 +0000

    stack heap

--- data_types/05_Summary.js
+++ data_types/05_Summary.js
@@ -54,3 +54,32 @@ const myFunction = function xyz(){
 console.log(typeof myFunction)  // expected output:- function
 console.log(myFunction);        // expected output:- [function : xyz]
 myFunction()                    // expected output:- hello world!
+
+
+// *****************************+++++++++++++++++++++++++++++++++
+
+// premitive data types uses stack memory that gives a copy of original value
+
+
+let myyoutube = "hitesh raj"
+let anotherfucttion = myyoutube
+anotherfucttion = "raajaji"
+console.log(myyoutube);             // expected output:- hitesh raj
+console.log(anotherfucttion);       // expected output:- rajaaji
+
+
+// non premitive data types uses heap memory that gives the reference of original value
+// any modification will be made to original value
+
+let myObj = {
+    name:"hitesh",
+    age:23
+}
+
+console.log(myObj);         // Expected output:- {name:'hitesh',age:23}
+
+let myObj2 = myObj;
+myObj2.name = "siddhant"
+
+console.log(myObj);         // Expected output:- {name:'siddhant',age:23}
+console.log(myObj2);        // Expected output:- {name:'siddhant',age:23}
