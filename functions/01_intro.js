ff871690c6406776c0f6d84d434df4a867090586
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-04 03:31:20 +0000

    function inrto

---
+++ functions/01_intro.js
@@ -0,0 +1,16 @@
+// function in javascript like very beautiful. we can do any operation with the help of function.
+// function are the named block of code that works when it is invoked.
+// fuction can be created using fuction keyword.
+
+function functionJavascript(){
+    console.log("This is my first code of function.");
+}
+// functionJavascript();
+
+// function can also have some parameters to perform operations on data.
+
+function secondFunction(num1,num2){
+    sum = num1 + num2;
+    return sum;
+}
+console.log(secondFunction(10,12));
