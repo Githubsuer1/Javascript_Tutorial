0927f8f63ee48b305b0144018c1421cab4771f90
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-04 23:08:46 +0000

    done

--- basics_02/03_strings.js
+++ basics_02/03_strings.js
@@ -1,4 +1,4 @@
-// strings are object in javascript
+// The String object is used to represent and manipulate a sequence of characters.
 // we can write it using single or double qoats.
 // we can declare it using two ways, literal and new keyword
 
@@ -13,10 +13,10 @@ console.log(`name is ${name} and age is ${age}`)        // Expected output:- nam
 //using new keyword
 
 //exp 2
-const courseName = new String("     MCA    ");
+const courseName = new String("  code-with-me  ");
 const cost = new String(1999);
 console.log(`coursName is ${courseName} and cost is ${cost}`);    // Expected output:- coursName is MCA and cost is 1999
-console.log(courseName.__proto__)       // Expected output:- {} but actualy it is not empty
+console.log(courseName.__proto__)                                 // Expected output:- {} but actualy it is not empty
 
 
 
@@ -25,12 +25,30 @@ console.log(courseName.__proto__)       // Expected output:- {} but actualy it i
 console.log(courseName.toLowerCase())          // Expected output:-mca
 console.log(courseName.trim());                // Expected output:- 'MCA'  
 
-console.log(courseName.slice(0,3));            // Expected output:- returns a slice of strin, does not modify original string
-console.log(courseName.slice(-8,3));           // Expected output:-"     ",   returns the value from back
+console.log(courseName.slice(0,3));            // Expected output:- returns a slice of string, does not modify original string
+console.log(courseName.slice(-8,3));           // Expected output:- "     ",   returns the value from last index
 
 const channelName = "code-with-harry-.-com";
 console.log(channelName.split("-"));           // Expected output:- returns an array of comma saparated value
 
+// A lot of methods are here that are used with strings is that 
 
+// String length()
+// String slice()
+// String substring()
+// String substr()
+// String replace()
+// String replaceAll()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String charAt()
+// String charCodeAt()
+// String split()
 
 
