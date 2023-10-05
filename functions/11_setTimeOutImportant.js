e26080cefa4daa7594633c1ad1486047074ff5ee
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-05 02:37:23 +0000

    setTimeOutCmplt

---
+++ functions/11_setTimeOutImportant.js
@@ -0,0 +1,12 @@
+for(var i=0; i<5; i++){
+    setTimeout(function(){
+        console.log(i);
+    },i*1000)
+}
+
+
+for(let i=0; i<5;i++){
+    setTimeout(function(){
+        console.log(i);
+    },i*1000)
+}
