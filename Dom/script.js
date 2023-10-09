1f7a34d0bab75114f4404ee9d960e3de9aa8e651
Siddhant Mishra <123529344+Githubsuer1@users.noreply.github.com>
2023-10-09 06:55:46 +0000

    Dom

---
+++ Dom/script.js
@@ -0,0 +1,14 @@
+const parant = document.querySelector('.parent');
+
+
+
+// console.log(parant);                         // Expected output:- parant class 
+// console.log(parant.children);                // Expected output:- htmlcollection of elements
+// console.log(parant.children[0].innerHTML);   // Expected output:- monday
+// console.log(parant.children[1].innerHTML);   // Expected output:- tuesday
+// console.log(parant.children[2].innerHTML);   // Expected output:- wednesday
+// console.log(parant.children[3].innerHTML);   // Expected output:- thursday
+
+console.log(parant.childNodes);
+console.log(parant.childElementCount);
+
